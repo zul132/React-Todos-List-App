e246730953f9d813e36d91e95ae11745c4430626
zul132 <fathimazulaikha.25cs@licet.ac.in>
2025-01-18 15:00:56 +0530

    Replaced the logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
 import React from 'react';
 
+import { AppBar, Box, IconButton, Paper, Toolbar, Typography } from '@mui/material';
+import Container from '@mui/material/Container';
+import { makeStyles } from '@mui/styles';
+
+/* We are going to add styles using JSS instead of CSS */
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    appContainer: {
+        paddingLeft: 100, /* default in pixels */
+        paddingRight: 100,
+        marginTop: 100,
+    },
+    wrapper: {
+        textAlign: "center",
+        width: "100%",
+    },
+}));
+
 export default function TTBTodos() {
+    const classes = useStyles();
+
     return (
-        <div>
-            <AppBar position="static">
+        <Box className={classes.root}>
+            <AppBar 
+                position="static"
+                sx={{
+                    backgroundColor: "#532ca0", // Replace with the exact purple color you want
+                }}
+            >
                 <Toolbar>
                     <IconButton edge="start" color="inherit">
                         <img  
-                            alt = "TickTickBoom Todos logo"
+                            alt="TickTickBoom Todos logo"
                             style={{
                                 width: "50px",
                             }} 
@@ -18,6 +42,15 @@ export default function TTBTodos() {
                     <Typography variant="h5">Todos</Typography>
                 </Toolbar>
             </AppBar>
-        </div>
+
+            {/* The Container component acts as a wrapper. It centers your content horizontally */}
+            <Container className={classes.appContainer}>
+                {/* The Paper component is a container for displaying content on an elevated surface */}
+                <Paper className={classes.wrapper} elevation={0}>
+                    {/* A component to add todos - "TodoAdder" */}
+                    {/* The container area to display all todos - "TodosContainer*/} 
+                </Paper>
+            </Container>
+        </Box>
     ); 
 }
